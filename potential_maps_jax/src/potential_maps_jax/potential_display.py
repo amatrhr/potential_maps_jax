7ac160d659693b655d8119a1b48fced1776023d1
charleslaurin <cl14022@outlook.com>
2025-04-17 17:00:51 -0700

    Adding basic plot abiliies; needs commenting and the new (negative-in-corner) class needs to be created

--- potential_maps_jax/src/potential_maps_jax/potential_display.py
+++ potential_maps_jax/src/potential_maps_jax/potential_display.py
@@ -1,15 +1,14 @@
 import jax
 from jax import random
 
-
-
 from jax import numpy as jnp
 import matplotlib.pyplot as plt
 from functools import partial
 
 
 class PotentialDisplay:
-    def __init__(self, gridsize: int = 32, numpoints: int = 20, numnegative: int = 1, seed:int = 0):
+    def __init__(self, gridsize: int = 32, numpoints: int = 20, numnegative: int = 1, seed: int = 0,
+                 quarternegative=False):
         self.GRID_SIZE = gridsize
         self.GRID_X, self.GRID_Y = jnp.indices([self.GRID_SIZE, self.GRID_SIZE], dtype=jnp.float32)
         self.GRID_X -= jnp.median(self.GRID_X)
@@ -24,14 +23,23 @@ class PotentialDisplay:
         key = jax.random.key(31212417 + seed)
         self.X_R = jax.random.choice(key, a=jnp.arange(-self.GRID_SIZE // 2, self.GRID_SIZE // 2, 1), replace=True,
                                      shape=(self.NUM_POINTS,))
-        self.Y_R = jax.random.choice(random.key(seed+1), a=jnp.arange(-self.GRID_SIZE // 2, self.GRID_SIZE // 2, 1),
+        self.Y_R = jax.random.choice(random.key(seed + 1), a=jnp.arange(-self.GRID_SIZE // 2, self.GRID_SIZE // 2, 1),
                                      replace=True, shape=(self.NUM_POINTS,))
         self.PARTICLE_LOCS = jnp.column_stack((self.X_R, self.Y_R))
         self.PARTICLE_CHGS = jnp.ones(self.NUM_POINTS)
+        quarter_negative_index = jnp.apply_along_axis(func1d=lambda x: (x[0] > 0) * (x[1] > 0), axis=1, arr=self.PARTICLE_LOCS)
         if numnegative > 0:
-            self.PARTICLE_CHGS = self.PARTICLE_CHGS.at[jax.random.choice(random.key(seed+41),
-                                               a=jnp.arange(len(self.PARTICLE_CHGS)), shape=(numnegative,))].set(-1)
-
+            negative_points = jax.random.choice(random.key(seed + 41),
+                                                a=jnp.arange(len(self.PARTICLE_CHGS)),
+                                                shape=(numnegative,))
+            if quarternegative:
+                print(self.PARTICLE_CHGS)
+                qn_update = jnp.array([negative_points[_] for _, x in enumerate(quarter_negative_index) if x])
+                self.PARTICLE_CHGS = self.PARTICLE_CHGS.at[qn_update].set(-1)
+                print(self.PARTICLE_CHGS)
+                print(self.PARTICLE_LOCS)
+            else:
+                self.PARTICLE_CHGS = self.PARTICLE_CHGS.at[negative_points].set(-1)
 
     def electric_field_of_particle(self, x0: jnp.ndarray, x0_charge: jnp.ndarray) -> jnp.ndarray:
         """
@@ -66,7 +74,7 @@ class PotentialDisplay:
             total_field += self.electric_field_of_particle(x0=particle_loc, x0_charge=self.PARTICLE_CHGS[idx])
         return total_field
 
-    def potential_of_particle(self, x0: jnp.ndarray, x0_charge: jnp.ndarray)-> jnp.ndarray:
+    def potential_of_particle(self, x0: jnp.ndarray, x0_charge: jnp.ndarray) -> jnp.ndarray:
         """
         FUnction to calculate the electric field of particle at all points
         :param x0:
@@ -83,7 +91,7 @@ class PotentialDisplay:
         # print(f'{r=}')
         mags = jnp.sqrt(xs ** 2 + ys ** 2)
         # print(f'{mags=}')
-        potential = x0_charge * (1 / mags)
+        potential = x0_charge * (-1 / mags)
         return potential
 
     #
@@ -99,25 +107,31 @@ class PotentialDisplay:
         total_potential = 0
         for idx, particle_loc in enumerate(self.PARTICLE_LOCS):
             total_potential += self.potential_of_particle(x0=particle_loc,
-                                                     x0_charge=self.PARTICLE_CHGS[idx])
+                                                          x0_charge=self.PARTICLE_CHGS[idx])
         return total_potential
 
-    def plot(self, contours=True):
+    def plot(self, contour=True, to_save=False, savename="potential_display.png"):
         fig, ax = plt.subplots(figsize=(16, 9), dpi=270)
         ax.set_aspect(1)
 
         partC = self.total_field_at_point()
         partD = self.potential_at_point()
 
-        if contours:
+        if contour:
             ctr0 = ax.contourf(self.COORD_X, self.COORD_Y, partD.T)
             plt.colorbar(ctr0, ax=ax)
 
-
         for _, j in enumerate(self.FLAT_X):
             ax.arrow(x=j, y=self.FLAT_Y[_], dx=partC[_, 0], dy=partC[_, 1])
-        pass
-        plt.savefig("potential_display.png")
-        plt.show()
 
+        if to_save:
+            plt.savefig(savename)
+
+        plt.show()
         plt.close()
+
+
+pd2 = PotentialDisplay(gridsize=32, numpoints=24, numnegative=8,quarternegative=False,seed=141)
+pd2.plot(to_save=True)
+pd2 = PotentialDisplay(gridsize=32, numpoints=24, numnegative=8,quarternegative=True,seed=141)
+pd2.plot(to_save=True, savename="qn_display.png")
